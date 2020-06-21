ed1e8abc6cbc70e8d0a532ac4a2718b9cf0d5df3
Anne Quinkenstein <a.quinkenstein@gmail.com>
2020-06-21 22:20:39 +0200

    put image in background and fixed game teaser circle appearance

--- src/components/Title.js
+++ src/components/Title.js
@@ -6,11 +6,10 @@ const style = false;
 
 function Title() {
   return (  
-    <div className='title'>
-      <div className={style? "arcadePic": "entrancePic"}>
-        <img src={background} />
-      </div>
-      <h1 className={style? "titleName": "nothing"}><i class="fa fa-long-arrow-left" aria-hidden="true"  onClick={()=> style = false}></i>  online Game Night</h1>
+    <>
+    <div className={style? "arcadeBackground": "entranceBackground"}>
+      <h1 className={style? "titleName": "nothing"}><i class="fa fa-long-arrow-left" aria-hidden="true"  onClick={()=> style = false}></i> online Game Night</h1>
+    </div> 
       <div className={style? "nothing": "gameTease"}> 
         <div className='hangman'>
           <img src='https://cdn2.momjunction.com/wp-content/uploads/2015/05/Hangman-Word-Game.jpg.webp' />
@@ -22,7 +21,7 @@ function Title() {
           <img src='http://chupacdn.s3.amazonaws.com/catalog/product/cache/1/thumbnail/1280x/17f82f742ffe127f42dca9de82fb58b1/h/a/hangman-game-with-animations-9828_imgs_9828.png' />
         </div>
       </div>
-    </div>
+    </>
   )
 }
 
