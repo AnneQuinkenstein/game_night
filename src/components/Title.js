30734544c439676d88e7b16f3a0f535d1d3b1996
Anne Quinkenstein <a.quinkenstein@gmail.com>
2020-06-22 07:38:50 +0200

    changes to text

--- src/components/Title.js
+++ src/components/Title.js
@@ -12,13 +12,14 @@ function Title() {
     </div> 
       <div className={style? "nothing": "gameTease"}> 
         <div className='hangman'>
-          <img src='https://cdn2.momjunction.com/wp-content/uploads/2015/05/Hangman-Word-Game.jpg.webp' />
+          HANGMAN
+          {/* <img src='https://cdn2.momjunction.com/wp-content/uploads/2015/05/Hangman-Word-Game.jpg.webp' /> */}
         </div>
         <div className='hangman'>
-          <img src='https://speakspeak.com/wp-content/uploads/2014/11/hangman-speakspeak-3-610x415.jpg' />
+          QUARTETT
         </div>
         <div className='hangman'>
-          <img src='http://chupacdn.s3.amazonaws.com/catalog/product/cache/1/thumbnail/1280x/17f82f742ffe127f42dca9de82fb58b1/h/a/hangman-game-with-animations-9828_imgs_9828.png' />
+          QUIZ
         </div>
       </div>
     </>
