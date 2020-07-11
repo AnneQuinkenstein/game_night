8ef99b28bcf92d220a7f1b6c1eb47843bd46b7cf
Anne Quinkenstein <a.quinkenstein@gmail.com>
2020-07-11 22:44:06 +0200

    changed css for hangman, refactored: got rid of pics & letters

--- src/components/Hangman/DisplayWord.js
+++ src/components/Hangman/DisplayWord.js
@@ -2,25 +2,22 @@ import React from 'react';
 import Letter from './Letter';
 
 const DisplayWord = (props) => {
-
-    const answer = () => {
-        
-        props.sentence.map((letter) => {
-            let letterState = '';
-            if (props.letter === ' ') {
-                letterState = ' ';
-            } else if (props.guessedLetters.includes(props.letter.toLowerCase())) {
-                letterState = props.letter
-            } else {
-                letterState = '_'
-            }
-            return letterState
+    
+    let letterState = '';
+    const answer = props.sentence.map(letter => {
+        if (letter === ' ') {
+            letterState = ' ';
+        } else if (props.guessedLetters.includes(letter.toLowerCase())) {
+            letterState = letter
+        } else {
+            letterState = '_'
         }
-    }
+        return letterState
+    });
 
     return (
         <div className="DisplayWord">
-            {answer()}
+            {answer}
         </div>
     )
 }
--- src/components/Hangman/GigsToDisplayAttempts/GifsList.js
+++ src/components/Hangman/GigsToDisplayAttempts/GifsList.js
@@ -1,12 +1,35 @@
-import React from 'react'; 
+import React, { Component } from 'react';
+import giphy_key from '../../../Keys';
 import Gif from './Gif';
 
-const GifList = (props) => {
+class GifsList extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      gifs: [],
+      cathegory: this.props.movieTitle,
+    }
+  }
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  fetchData = () => {
+    fetch(`https://api.giphy.com/v1/gifs/search?q=${this.state.cathegory}&tag=movie&api_key=${giphy_key}&limit=5`)
+      .then(res => res.json())
+      .then(data => this.setState({ gifs: data.data }))
+  }
+
+
+  render() {
     return (
-        <div className="GifsList">
-            { props.gifs.map((gif, index) => <Gif gifURL={gif.images.downsized_large.url} falseGuesses={props.falseGuesses} index={index} key={gif.images.downsized_large.url}/>) } 
-        </div>
-    )
+      <div className="GifsList">
+        {this.state.gifs.map((gif, index) => <Gif gifURL={gif.images.downsized_large.url} falseGuesses={this.props.falseGuesses} index={index} key={gif.images.downsized_large.url} wrongLetter={this.props.wrongLetters[index]} />)}
+      </div>
+    );
+  }
 }
 
-export default GifList; 
+export default GifsList;
