716bc720ce307bc396f7ac12f847c2ffedecbf0b
Anne Quinkenstein <a.quinkenstein@gmail.com>
2020-07-11 15:00:29 +0200

    hangman displayed with pics, start second hangman with fortuneboard

--- src/components/Hangman/Hangman.js
+++ src/components/Hangman/Hangman.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import { Component } from 'react';
 import DisplayWord from './DisplayWord';
-import './Hangman.css';
 import Guess from './Guess';
-import Pics from './DisplayAttempts/Pics'; 
-import WrongLetters from './WrongLetters'; 
+import Pics from './GigsToDisplayAttempts/Pics';
+import './Hangman.css';
 
-const movieTitle = "The Snowpiercer"
+const movieTitle = "Magnolia"
 
 class Hangman extends Component {
     
@@ -15,11 +14,12 @@ class Hangman extends Component {
         this.state = {
             guessedLetters: [],
             sentence: movieTitle.split(''),
-            remainingGuesses: 5,
+            Guesses: 5,
             wrongLetters : [],
          }
     }
 
+    // Array of GuessedLetters
     updateGuessedLetters = (letter) => {
         letter = letter.toLowerCase();
         if (this.state.guessedLetters.includes(letter)){
@@ -29,30 +29,30 @@ class Hangman extends Component {
             guessedLetters : [...this.state.guessedLetters, letter]
             })
         }
+    }    
+
+    // Array of WronglyGussedLetters 
+    getWronglyGuessedLetters = () => {
+        const wrongLetters = this.state.guessedLetters.filter(letter => {
+            return !this.state.sentence.includes(letter) && !this.state.sentence.includes(letter.toUpperCase())
+        })
+        return wrongLetters; 
     }
 
-    
-    updateRemainingGuesse = (letter) => {
-        if (!this.state.guessedLetters.includes(letter) && !this.state.sentence.includes(letter)) {
-           this.setState({
-            remainingGuesses : this.state.remainingGuesses -1, 
-           })
-        }
-    }      
-
-    // getWrongLetters = () => {
-    //     setState(wrongLetters : this.state.guessedLetters.filter(letter => {
-    //         return !this.state.sentence.includes(letter)
-    //     }))
-    // }
+    // Num Remaing Guesses 
+    getRemainingGuesses = () => {
+        const remainingGuesses = this.state.Guesses - this.getWronglyGuessedLetters().length;     
+        return remainingGuesses;
+    }
 
     render(){
         return(
             <div className="Hangman">
+                {/* <h1>Find the hidden sentence! Guess a letter! You got 5 Lives. If you dye, you will have a glance to the world you are searching for. </h1> */}
+                <Pics movieTitle={movieTitle} falseGuesses={this.getWronglyGuessedLetters().length}/>
                 <DisplayWord sentence={this.state.sentence} guessedLetters={this.state.guessedLetters}/>
-                <Guess updateGuessedLetters={this.updateGuessedLetters} getWrongLetters={this.getWrongLetters} />
-                {/* <h1>{wrong}</h1> */}
-                {/* <Pics movieTitle={movieTitle}/> */}
+                <Guess updateGuessedLetters={this.updateGuessedLetters} />
+                <h1>{this.getWronglyGuessedLetters()}</h1> 
             </div>
         )
     }
--- src/components/Title.js
+++ src/components/Title.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import background from '../arcadeEntrance.jpg';
 import pacghost from '../blinky.png';
 import axios from 'axios';
+import { Link } from 'react-router-dom'; 
 
 
 class Title extends Component {
@@ -32,16 +33,16 @@ class Title extends Component {
 
   render() {
     return (  
-      <>
+      <div className="Title">
       <div className={this.state.style? "arcadeBackground": "entranceBackground"}>
         <h1 className={this.state.style? "titleName": "nothing"}><i class="fa fa-long-arrow-left" aria-hidden="true"  onClick={this.handleClick}></i> <span onClick={this.handleClick}>online Game Night</span></h1>
-        <h1 className={this.state.style? "nothing": "jokes"} onMouseEnter={this.changeJoke}> <span onMouseEnter={this.changeJoke}>What</span> <span onMouseEnter={this.changeJoke}> is so </span> <span onMouseEnter={this.changeJoke}>funny</span> about <span onMouseEnter={this.changeJoke}>... </span></h1>
+        <h1 className={this.state.style? "nothing": "jokes"} onClick={this.changeJoke}> <span onClick={this.changeJoke}>What</span> <span onClick={this.changeJoke}> is so </span> <span onClick={this.changeJoke}>funny</span> about <span onClick={this.changeJoke}>... </span></h1>
         <p className={this.state.style? "nothing": "jokes"}>{this.state.joke}</p>
       </div> 
         <div className={this.state.style? "nothing": "gameTease"}> 
           <div className='hangman'>
             <img src={pacghost} /> 
-            <div class="center">HANGMAN</div>
+            <div class="center"><Link to='/hangman'>HANGMAN</Link></div>
           </div>
           <div className='hangman'>
             <img src={pacghost} /> 
@@ -52,7 +53,7 @@ class Title extends Component {
           <div class="center"> QUIZ</div>
           </div>
         </div>
-      </>
+      </div>
     )
   }
 }
