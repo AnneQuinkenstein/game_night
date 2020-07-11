4b118b9cb7a9818fd16f368175b87a57f3e0906f
Anne Quinkenstein <a.quinkenstein@gmail.com>
2020-07-11 20:14:47 +0200

    classic hangman changes, deleated letters to move loop to displayword

--- src/Keys.js
+++ src/Keys.js
@@ -1,3 +1,5 @@
 const giphy_key = 'PttLTE6h2JMKFC00mSxFaJAUdfZhcmrI'; 
+const themoviedb_key='4db4144033ef5a34afbec19191f494c4';
 
 export default giphy_key; 
+// export default themoviedb_key;
---
+++ src/components/Hangman/ExplainHangman.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Component } from 'react';
+
+class ExplainHangman extends Component {
+
+    render() {
+        return (
+            <div>
+                <h1>Find the Movie Title! Guess a letter! You got 5 Lives. If you dye, you will have a glance to the world you are searching for.
+
+                We are looking for a movie Title from the year ... - choose the year
+                Guess a letter. If the letter is in the Movie, if will be displayes. If not: you will get a hint. A picture which is associated to the movie title is displayed. But you loose one of your five Lives. 
+                </h1>
+            </div>
+        )
+    }
+
+}
+
+export default ExplainHangman; 
