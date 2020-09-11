7930e62731a692ca078269e3e1f531856f2e5662
Anne Quinkenstein <a.quinkenstein@gmail.com>
2020-09-11 23:47:13 +0200

    added about me text

--- src/components/Contact/Text.js
+++ src/components/Contact/Text.js
@@ -7,9 +7,10 @@ const Text = () => {
                 <div id="noise"></div>
                 <h1>About me</h1>
                 <div className="aboutMeLine"></div>
-                <p>this that this that this that this that this that this that this that this that v v this that this thatthis thatthis that</p>
-                <p>this that this that this that this that this that this that this that this that this that this that this that this that this that this that this that this that </p>
-                <p>this that this that this that this that this that this that this that this that this that this that this that this that this that this that this that this that</p>
+                <p>Once upon a time, i started coding on an old Schneider CPC 464 with such a green and black monitor as this. As a kid i loved to manipulate Pacman & build new paths and behaviour in Basic.</p>
+                <p>As a studend i used Ubuntu. That turned me into a technical customer service for all of my friends. I was called when someone had a problem with their computer - or any other electronical device. Most of the time it could be solved by turning it off and on. I liked beeing admired for that. </p>
+                <p>There was a time, where i did a bit of basic static HTML, CSS Webpages or used Wordpress e.g. for some classes i gave. We build a network with Samba in a shared house, that was exciting. </p>
+                <p>And after a while i decided to give up my former job to change into Softwaretesting. I loved pairing with a devloper, trying to get a hold of a bug. But as the organisation grow i got more and more into developing the QA department. Because i missed the code i started a Frond-End Development Course during Covid19-Lockdown and fall into love with coding again. And now here we are - what can i do? Leave me a message.</p>
             </div >
         </div>
     )
