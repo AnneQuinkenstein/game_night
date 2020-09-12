42eaf3bfbbbafa4270c79e920d6a5a978b938c64
Anne Quinkenstein <a.quinkenstein@gmail.com>
2020-09-12 06:38:39 +0200

    changged typo in rules

--- src/components/Contact/Text.js
+++ src/components/Contact/Text.js
@@ -9,8 +9,8 @@ const Text = () => {
                 <div className="aboutMeLine"></div>
                 <p>In early ages I started coding on an old Schneider CPC 464 with such a green and black monitor as this. As a kid I loved to manipulate Pacman & build new paths and behaviour in Basic.</p>
                 <p>As a studend I used Linux. That turned me into a technical customer service for all of my friends. I was called when someone had a problem with their computer - or any other electronical device. Most of the time it could be solved by turning it off and on. I liked beeing admired for that. </p>
-                <p>There was a time, where I did a bit of basic static HTML, CSS Webpages or used Wordpress e.g. for some classes i gave. We build a network with Samba in a shared house, that was exciting. </p>
-                <p>And after a while I decided to give up my former job to change into Software Testing. I loved pairing with a developer, trying to get a hold of a bug. But as the organisation grow I got more and more into developing the QA department. Because I missed the code I started a Frond-End Development Course during Covid19-Lockdown and fall into love with coding again. And now here we are - what can i do? Leave me a message.</p>
+                <p>There was a time, where I did a bit of basic static HTML, CSS Webpages or used Wordpress e.g. for some classes I gave. We build a network with Samba in a shared house, that was exciting. </p>
+                <p>And after a while I decided to give up my former job to change into Software Testing. I loved pairing with a developer, trying to get a hold of a bug. But as the organisation grow, I got more and more into developing the QA department. Because I missed the code I started a Frond-End Development Course during Covid19-Lockdown and fall into love with coding again. And now here we are - what can i do? Leave me a message.</p>
             </div >
         </div>
     )
